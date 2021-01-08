f2bb3fe15f73ee0c22e4dd88fb08f5405d9fc358
Yuliya Kryvorot <krviua@yahoo.com>
2021-01-08 14:36:10 +0300

    feat: add wallet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './styles/main.scss';
 
+import { createModalElement, receiveIncomeModal } from './script/components/modal';
 import expenses from './script/components/expenses';
+import createWallet from './script/components/wallet';
 
+const modalIncome = createModalElement(receiveIncomeModal, 'receive-income');
+document.body.append(modalIncome);
+createWallet();
 expenses();
---
+++ src/script/components/wallet.js
@@ -0,0 +1,16 @@
+import createElement from '../utils/create';
+
+export default function createWallet() {
+  const walletTitle = createElement('h3', 'wallet__title', 'Wallet');
+  const incomeReceived = createElement(
+    'button',
+    ['btn', 'btn-success'],
+    null,
+    ['bsToggle', 'modal'],
+    ['bsTarget', '#receive-income'],
+  );
+
+  const walletDiv = createElement('div', ['container-sm', 'wallet'], [walletTitle, incomeReceived]);
+
+  document.querySelector('.transactions-dashboard').append(walletDiv);
+}
