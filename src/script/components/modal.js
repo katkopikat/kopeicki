6180ef450faf5f45928a66f67e923a8647fc4329
Yuliya Kryvorot <krviua@yahoo.com>
2021-01-09 01:08:05 +0300

    feat: add styles to expense modal

--- src/script/components/modal.js
+++ src/script/components/modal.js
@@ -11,27 +11,24 @@ export function createModal(modalHtml, id) {
 
 export function addExpenseModal(category) {
   return `<div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content expense-modal">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">What did you spend money on?</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <p>${category}</p>
-            <input type="number"></input>
-            <span id="currency">BYN</span>
-            <label for="date">Choose date:</label>
-            <input type="date" id="date"
+            <h5 class="modal-body__title">SPEND</h5>
+            <p><span class="modal-body__amount" role="textbox" contenteditable></span>BYN</p>
+            <span class="modal-body__category-name">at #${category}</span>
+            <input type="date" class="modal-body__date"
               value=${new Date().toISOString().split('T')[0]}
               min="2018-01-01" max=${new Date().toISOString().split('T')[0]} >
             <div class="form-floating">
               <textarea class="form-control" placeholder="Leave a descrioption here" id="description"></textarea>
-              <label for="description">Description</label>
+              <label for="description" class="textarea-label">Do you have any comments?</label>
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn">Wasted!</button>
           </div>
         </div>
       </div>
