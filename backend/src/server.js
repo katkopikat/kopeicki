8cee27985ed8df325a382059a4b80800a9918192
vof <vof1111@gmail.com>
2021-01-08 16:00:47 +0300

    fix: eslint config error

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,18 @@
-import app from './app.js';
 import mongoose from 'mongoose';
+import app from './app.js';
 
 const PORT = 8000;
-const MONGO_CONNECTION_STRING = 'mongodb+srv://admin:7FITZQDF5qpnQbRS@rscluster.msaso.mongodb.net/rsclone?retryWrites=true&w=majority'
+const MONGO_CONNECTION_STRING = 'mongodb+srv://admin:7FITZQDF5qpnQbRS@rscluster.msaso.mongodb.net/rsclone?retryWrites=true&w=majority';
 
 mongoose.connect(MONGO_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 const db = mongoose.connection;
 
 db.on('error', () => console.log('MongoDB connection error'));
 db.once('open', () => {
   console.log('connected to mongoDB');
-  app.listen(PORT, () =>
-    console.log(`App is running on http://localhost:${PORT}`)
-  );
+  app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
 });
-
