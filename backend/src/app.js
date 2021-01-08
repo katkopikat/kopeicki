607fa9fcf3fa7dc5931df8edda31f25b8d7b7581
vof <vof1111@gmail.com>
2021-01-08 18:53:27 +0300

    feat: add cors middleware to support cross-origin requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import transactionRouter from './resources/transactions/transaction.router.js';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+
+app.options('*', cors());
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
