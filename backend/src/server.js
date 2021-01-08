8ae10552caed9e2ba1817ac688922cba781469ec
vof <vof1111@gmail.com>
2021-01-08 15:41:01 +0300

    feat: add connection to mongoDB via mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,21 @@
 import app from './app.js';
+import mongoose from 'mongoose';
 
 const PORT = 8000;
-app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
+const MONGO_CONNECTION_STRING = 'mongodb+srv://admin:7FITZQDF5qpnQbRS@rscluster.msaso.mongodb.net/rsclone?retryWrites=true&w=majority'
+
+mongoose.connect(MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+const db = mongoose.connection;
+
+db.on('error', () => console.log('MongoDB connection error'));
+db.once('open', () => {
+  console.log('connected to mongoDB');
+  app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+});
+
