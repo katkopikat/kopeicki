323ec5f25a8f0b4ba2fd06efa57963862272924a
vof <vof1111@gmail.com>
2021-01-12 19:15:49 +0300

    feat: use env vars for storing app secure config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,8 @@
+import 'dotenv/config.js';
 import mongoose from 'mongoose';
 import app from './app.js';
 
-const PORT = process.env.PORT || 8000;
-const MONGO_CONNECTION_STRING = 'mongodb+srv://admin:7FITZQDF5qpnQbRS@rscluster.msaso.mongodb.net/rsclone?retryWrites=true&w=majority';
-
-mongoose.connect(MONGO_CONNECTION_STRING, {
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -14,5 +12,5 @@ const db = mongoose.connection;
 db.on('error', () => console.log('MongoDB connection error'));
 db.once('open', () => {
   console.log('connected to mongoDB');
-  app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
+  app.listen(process.env.PORT, () => console.log(`App is running on http://localhost:${process.env.PORT}`));
 });
