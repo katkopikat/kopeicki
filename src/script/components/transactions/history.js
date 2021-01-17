a078c80ed7f5ef1e53aa30ba4e592ec65763895e
Yuliya Kryvorot <krviua@yahoo.com>
2021-01-17 23:39:33 +0300

    feat: implement history rendering

---
+++ src/script/components/transactions/history.js
@@ -0,0 +1,85 @@
+import app from '../../app';
+import createElement from '../../utils/create';
+
+const sortByDays = (list) => {
+  const newList = [];
+
+  for (let i = 0; i < list.length; i += 1) {
+    if (!newList.length) {
+      newList.push([list[i]]);
+    } else {
+      newList.forEach((item) => {
+        if (item[0].date === list[i].date) {
+          item.push(list[i]);
+        } else {
+          newList.push([list[i]]);
+        }
+      });
+    }
+  }
+  return newList;
+};
+
+const formatDate = (date) => {
+  const d = date.getDate();
+  const m = date.getMonth() + 1;
+  const y = date.getFullYear();
+  return `${y}-${m <= 9 ? `0${m}` : m}-${d <= 9 ? `0${d}` : d}`;
+};
+
+export default async function renderHistory() {
+  const dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
+
+  const transactions = await app.api.getTransactions();
+  const last5transactions = transactions.slice(0, 5);
+  const days = sortByDays(last5transactions);
+
+  days.forEach((day) => {
+    const dateDiv = createElement('div');
+    const dayDiv = createElement('div', 'day');
+
+    const today = formatDate(new Date());
+    const yesterday = formatDate(new Date(new Date().setDate(new Date().getDate() - 1)));
+    const date = new Date(day[0].date);
+    const formattedDate = new Intl.DateTimeFormat('en-GB', dateOptions).format(date);
+
+    let dayOfWeek;
+
+    if (today === formatDate(date)) {
+      dayOfWeek = 'today';
+    } else if (yesterday === formatDate(date)) {
+      dayOfWeek = 'yesterday';
+    } else {
+      [dayOfWeek] = formattedDate.split(',');
+    }
+
+    dateDiv.insertAdjacentHTML(
+      'beforeend',
+      `
+        <span class="day-of-week">${dayOfWeek},</span>
+        <span class="date">${formattedDate.split(',')[1]}</span>
+      `,
+    );
+
+    day.forEach((transaction) => {
+      dayDiv.insertAdjacentHTML(
+        'beforeend',
+        `
+        <div class="row">
+        <div class="col">
+          <p class="account-name">${transaction.account}</p>
+          <p class="category-name">${transaction.category}</p>
+        </div>
+        <div class="col">
+          <p class="money-amount ${transaction.type}">${transaction.amount}</p>
+          <p class="description">${transaction.description}</p>
+        </div>
+      </div>
+        `,
+      );
+    });
+
+    const container = createElement('div', '', [dateDiv, dayDiv]);
+    document.querySelector('.transactions-history').append(container);
+  });
+}
--- src/script/components/transactions/transactions.js
+++ src/script/components/transactions/transactions.js
@@ -1,25 +1,14 @@
 import createElement from '../../utils/create';
 import { dragStart, dragEnd, dragOver, dragEnter, dragLeave, dragDrop } from './dragnDrop';
 import createCategoryList from './categories';
+import renderHistory from './history';
 
 export default function renderTransactionsPage(/* options */) {
-  const accountsDiv = createElement(
-    'div',
-    'transactions-dashboard__item accounts',
-    '<h4>Accounts</h4>',
-  );
-  const expensesDiv = createElement(
-    'div',
-    'transactions-dashboard__item expenses',
-    '<h4>Expenses</h4>',
-  );
+  const accountsDiv = createElement('div', 'transactions-dashboard__item accounts', '<h4>Accounts</h4>');
+  const expensesDiv = createElement('div', 'transactions-dashboard__item expenses', '<h4>Expenses</h4>');
   const incomeDiv = createElement('div', 'transactions-dashboard__item income', '<h4>Income</h4>');
 
-  const dashboard = createElement('div', 'col-7 transactions-dashboard', [
-    accountsDiv,
-    expensesDiv,
-    incomeDiv,
-  ]);
+  const dashboard = createElement('div', 'col-7 transactions-dashboard', [accountsDiv, expensesDiv, incomeDiv]);
   const history = createElement('div', 'col-4 transactions-history');
 
   const row = createElement('div', 'row', [dashboard, history]);
@@ -31,6 +20,8 @@ export default function renderTransactionsPage(/* options */) {
   createCategoryList('accounts', accountsDiv);
   createCategoryList('expenses', expensesDiv);
   createCategoryList('income', incomeDiv);
+
+  renderHistory();
 
   const draggables = document.querySelectorAll('[draggable="true"]');
   const accounts = document.querySelectorAll('[draggable="false"]');
