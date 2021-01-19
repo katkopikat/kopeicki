c77d2c7e7aceac6a4873a2bc967a4265035de0e8
Yuliya Kryvorot <krviua@yahoo.com>
2021-01-19 13:54:24 +0300

    feat: add transition while switching theme

--- src/script/components/settings/theme.js
+++ src/script/components/settings/theme.js
@@ -4,13 +4,20 @@ export default function switchTheme() {
   themeToggle.addEventListener('change', () => {
     const ball = document.querySelector('.ball');
 
+    const transition = () => {
+      document.documentElement.classList.add('transition');
+      window.setTimeout(() => {
+        document.documentElement.classList.remove('transition');
+      }, 1000);
+    };
+
     if (themeToggle.checked) {
       ball.style.transform = 'translateX(40px)';
-
+      transition();
       document.documentElement.setAttribute('theme', 'light');
     } else {
       ball.style.transform = 'translateX(0)';
-
+      transition();
       if (document.documentElement.hasAttribute('theme')) {
         document.documentElement.removeAttribute('theme');
       }
