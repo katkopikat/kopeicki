62d101ca4923d10a9270a3e7ea0d924d55ed2452
Yuliya Kryvorot <krviua@yahoo.com>
2021-01-26 10:20:42 +0300

    refactor: delete unused files

--- src/script/components/settings/currency.js
+++
@@ -1,7 +0,0 @@
-import { getCurrencieslist, renderCurrencyList } from './currency_list';
-
-const endpointCurrencyList = 'https://free.currconv.com/api/v7/currencies?apiKey=9907c9f725cc3b503486';
-
-export default function displayCurrencyList() {
-  getCurrencieslist(endpointCurrencyList).then(() => renderCurrencyList());
-}
