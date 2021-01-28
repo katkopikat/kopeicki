9ca47181f2f76c27865d3df4072faabf48455847
Yuliya Kryvorot <krviua@yahoo.com>
2021-01-28 22:58:53 +0300

    feat: implement confirm modal

---
+++ src/script/components/modals/confirmModal.js
@@ -0,0 +1,70 @@
+import createElement from '../../utils/create';
+import modal from './modal';
+import app from '../../app';
+import { getLanguage } from '../../utils/localStorage';
+
+export default function confirmModal(group, category) {
+  const lang = getLanguage();
+
+  const msgCategory = {
+    en: 'Delete category?',
+    ru: 'Удалить категорию?',
+    be: 'Выдаліць катэгорыю?',
+  };
+
+  const msgTransaction = {
+    en: 'Delete transaction?',
+    ru: 'Удалить операцию?',
+    be: 'Выдаліць аперацыю?',
+  };
+
+  const msgYes = {
+    en: 'Yes',
+    ru: 'Да',
+    be: 'Так',
+  };
+
+  const msgNo = {
+    en: 'No',
+    ru: 'Нет',
+    be: 'Не',
+  };
+
+  document.querySelector('.modal-content').className = 'modal-content warning';
+
+  const wrap = createElement('div', 'content');
+
+  wrap.insertAdjacentHTML(
+    'afterbegin',
+    `<h5 class="modal-body__title">${msgCategory[lang]}</h5>
+        <div class="btns-container">
+          <button class="btn" data-msg="confirm">${msgYes[lang]}</button>
+          <button class="btn" data-msg="reject">${msgNo[lang]}</button>
+        </div>`,
+  );
+
+  const buttons = wrap.querySelector('.btns-container');
+
+  buttons.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      const isConfirmed = e.target.dataset.msg === 'confirm';
+      if (isConfirmed) {
+        switch (group) {
+          case 'accounts':
+            app.removeUserAccount(category);
+            break;
+          case 'expenses':
+            app.removeUserExpense(category);
+            break;
+          case 'income':
+            app.removeUserIncome(category);
+            break;
+          // no default
+        }
+      }
+      modal.hide();
+    }
+  });
+
+  return wrap;
+}
