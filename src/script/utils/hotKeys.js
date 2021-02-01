8a39c1acd0e34282e49801dd8aba034d68c91eb3
Yuliya Kryvorot <krviua@yahoo.com>
2021-02-01 12:52:12 +0300

    fix: restrict hot keys in login page

--- src/script/utils/hotKeys.js
+++ src/script/utils/hotKeys.js
@@ -18,24 +18,26 @@ export default function hotKeys() {
   document.addEventListener('keydown', (e) => {
     pressed.add(e.code);
 
-    if (pressed.size === 2 && e.altKey) {
-      if (pressed.has('KeyT')) pubsub.publish('navigateTo', '/');
-      if (pressed.has('KeyS')) pubsub.publish('navigateTo', '/statistics');
-      if (pressed.has('KeyH')) pubsub.publish('navigateTo', '/history');
-      if (pressed.has('KeyE')) {
-        modal.setContent(transactionModal({ type: 'expenses' }));
-        modal.show();
-      }
-      if (pressed.has('KeyI')) {
-        modal.setContent(transactionModal({ type: 'income' }));
-        modal.show();
-      }
-      if (pressed.has('KeyA')) {
-        modal.setContent(accountModal({ type: 'accounts' }));
-        modal.show();
-      }
+    if (!window.location.href.includes('login')) {
+      if (pressed.size === 2 && e.altKey) {
+        if (pressed.has('KeyT')) pubsub.publish('navigateTo', '/');
+        if (pressed.has('KeyS')) pubsub.publish('navigateTo', '/statistics');
+        if (pressed.has('KeyH')) pubsub.publish('navigateTo', '/history');
+        if (pressed.has('KeyE')) {
+          modal.setContent(transactionModal({ type: 'expenses' }));
+          modal.show();
+        }
+        if (pressed.has('KeyI')) {
+          modal.setContent(transactionModal({ type: 'income' }));
+          modal.show();
+        }
+        if (pressed.has('KeyA')) {
+          modal.setContent(accountModal({ type: 'accounts' }));
+          modal.show();
+        }
 
-      pressed.clear();
+        pressed.clear();
+      }
     }
   });
 
